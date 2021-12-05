d520328bf3938e2db3bbd056ecef6bf50323fc50
Diego Pagini <diegopaginidev@gmail.com>
2021-12-05 21:53:51 +0100

    deploy

--- src/app/components/card/card.module.ts
+++ src/app/components/card/card.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { CardComponent } from './card.component';
 import { MaterialModule } from 'src/app/shared/material/material.module';
 import { PipesModule } from 'src/app/pipes/pipes.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [CardComponent],
-  imports: [CommonModule, MaterialModule, PipesModule],
+  imports: [CommonModule, MaterialModule, PipesModule, RouterModule],
   exports: [CardComponent],
 })
 export class CardModule {}
